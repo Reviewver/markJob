4cd0d95f431e21598ec7c94565e749a0af6d03b4
simon <simon.hemery.56350@gmail.com>
2018-10-12 23:23:37 +0200

    Ajout de la page avec le titre sur l'enregistrement des demandes

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Enregistrement des demandes envoyées au entreprise</title>
+<link rel="stylesheet" type="text/css" href="bootstrap-4.1.3-dist/css/bootstrap.css"/>
+</head>
+<body>
+<header>
+<h1>Enregistrement des demandes envoyées au entreprise</h1>
+</header>
+</body>
+</html>
