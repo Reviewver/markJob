56827172e87a78159149596c082ba09e21d760af
Hemery Simon <simon.hemery.56350@gmail.com>
2019-01-23 18:03:38 +0100

    Séparation du fichier de connection à la base de donnée de l'autre fichier

--- connect.php
+++ connect.php
@@ -1,7 +1,5 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=base_envoie_cv";
-$user = "web";
-$pass = "web";
+include("db.php");
 
 try {
     $connect_mysql = new PDO($dsn, $user, $pass);
---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=base_envoie_cv";
+$user = "web";
+$pass = "web";
+?>
