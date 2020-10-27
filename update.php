d79ffe8f57b71fd70c57aeb881428473a02586fb
Simon Hemery <simon.hemery.56350@gmail.com>
2020-10-27 18:39:06 +0100

    Ajout d'une vue pour la page de mise à jour d'une demande

--- update.php
+++ update.php
@@ -1,34 +1,3 @@
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Enregistrement des demandes envoyées au entreprise</title>
-		<link rel="stylesheet" type="text/css" href="bootstrap-4.1.3-dist/css/bootstrap.css"/>
-		<link rel="stylesheet" type="text/css" href="index.css"/>
-	</head>
-    <body>
-		<div class="container">
-        <header>
-				<h1>Mise à jour des demandes envoyées à l'entreprise <?php echo $_POST['name'] ?></h1>
-				<?php include("menu.php"); ?>
-			</header>
-            <form class="row" action="save_update.php" method="post">
-			<input type="hidden" name="id" value="<?php echo "".$_POST['id']."" ?>"></input>
-			<div class="form-groupe col-sm-12">
-			<label for="reponse">Réponse</label>
-            <textarea class="col-sm-12" type="text" name="reponse" id="reponse"></textarea>
-			</div>
-			<div class="form-groupe col-sm-12">
-			<label for="oui">Oui</label>
-			<input name="statut" type="radio" value="2" id="oui">
-			<label for="non">Non</label>
-			<input name="statut" type="radio" value="1" id="non">
-			</div>
-			<div class="form-groupe col-sm-12">
-			<label for="date">Date</label>
-			<input name="date" id="date" value="<?php echo date("Y-m-d") ?>" type="date">
-			</div>
-			<button class="btn btn-primary" type="submit">Enregistrer</button>
-            </form>
-        </div>
-	</body>
- </html>
+<?php
+include "viewUpdateDemande.php";
+?>
---
+++ viewUpdateDemande.php
@@ -0,0 +1,27 @@
+<?php 
+$titre = 'EnvoieCV';
+$subtitle = 'Mise à jour de demande';
+?>
+<?php ob_start(); ?>
+		<div class="container">
+            <form class="row" action="save_update.php" method="post">
+			<input type="hidden" name="id" value="<?php echo "".$_POST['id']."" ?>"></input>
+			<div class="form-groupe col-sm-12">
+			<label for="reponse">Réponse</label>
+            <textarea class="col-sm-12" type="text" name="reponse" id="reponse"></textarea>
+			</div>
+			<div class="form-groupe col-sm-12">
+			<label for="oui">Oui</label>
+			<input name="statut" type="radio" value="2" id="oui">
+			<label for="non">Non</label>
+			<input name="statut" type="radio" value="1" id="non">
+			</div>
+			<div class="form-groupe col-sm-12">
+			<label for="date">Date</label>
+			<input name="date" id="date" value="<?php echo date("Y-m-d") ?>" type="date">
+			</div>
+			<button class="btn btn-primary" type="submit">Enregistrer</button>
+            </form>
+        </div>
+<?php $contenu = ob_get_clean(); ?>
+<?php require 'gabaris.php'; ?>
