20cb31a4534ae89c9ce04fa81b35090e402e02e0
Simon Hemery <simon.hemery.56350@gmail.com>
2020-10-27 19:39:39 +0100

    Ajout de la fonction insertMiseAJouEntreprise dans le model

--- model.php
+++ model.php
@@ -73,4 +73,30 @@ function suppression_company($ID,$connect)
         return 1;
         
 }
+
+function insertMiseAJourEntreprise($id, $response, $statut, $date)
+{ 
+	try {
+	include("connect.php");
+	$requete = "INSERT INTO miseajour_entreprise VALUES (:id_lien, :date_update, :statut, :justification)";
+
+	if(!$prepare_requete = $connect_mysql->prepare($requete))
+	{
+		echo "Erreur";
+	}
+
+	if(!$prepare_requete->execute(array(':id_lien' => $id, ':date_update'=> $date, ':statut' => $statut, ':justification' => $reponse)))
+	{
+		echo "Erreur";
+	}
+	else
+	{
+		 header('Location: liste_demande.php');
+	}
+
+	} catch (PDOException $e) {
+		print "Erreur" . $e->getMessage() . "<br/>";
+		die();
+	}
+}
 ?>	
--- save_update.php
+++ save_update.php
@@ -1,31 +1,9 @@
-<?php 
-try {
-include("connect.php");
-$requete = "INSERT INTO miseajour_entreprise VALUES (:id_lien, :date_update, :statut, :justification)";
-
-if(!$prepare_requete = $connect_mysql->prepare($requete))
-{
-	echo "Erreur";
-}
-
-$id = htmlspecialchars($_POST['id']);
-$reponse = htmlspecialchars($_POST['reponse']);
-$statut = htmlspecialchars($_POST['statut']);
-$date = htmlspecialchars($_POST['date']);
-
-
-
-if(!$prepare_requete->execute(array(':id_lien' => $id, ':date_update'=> $date, ':statut' => $statut, ':justification' => $reponse)))
-{
-	echo "Erreur";
-}
-else
-{
-	 header('Location: liste_demande.php');
-}
-
-} catch (PDOException $e) {
-	print "Erreur" . $e->getMessage() . "<br/>";
-	die();
-}
+<?php
+include "model.php";
+insertMiseAJourEntreprise(
+htmlspecialchars($_POST['id']),
+htmlspecialchars($_POST['reponse']),
+htmlspecialchars($_POST['statut']),
+htmlspecialchars($_POST['date'])
+);
 ?>
